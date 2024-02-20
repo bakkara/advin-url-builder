03bbf30ffa52270921d403c8ff0ac19112cf9ab3
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-02-20 18:52:44 +0200

    add form with params

--- script.js
+++ script.js
@@ -36,3 +36,34 @@ function generateURL(buttonType) {
     outputUrl.innerHTML = `<p>Згенерований URL: <a href="${urlObject.toString()}" target="_blank">${urlObject.toString()}</a></p>`;
 }
 
+function generateURLWithParams(buttonType) {
+    const urlInputWithParams = document.querySelector('.urlInputWithParams').value;
+    let sourceInputWithParams = document.querySelector('.sourceInputWithParams').value;
+    let mediumInputWithParams = document.querySelector('.mediumInputWithParams').value;
+    let contentInputWithParams = document.querySelector('.contentInputWithParams').value;
+    
+    const outputUrlWithParams = document.querySelector('.outputUrlWithParams');
+    const urlObjectWithParams = new URL(urlInputWithParams);
+
+    const content = contentInputWithParams || urlObjectWithParams.searchParams.get('android') || '';
+    const source = sourceInputWithParams || urlObjectWithParams.searchParams.get('client');
+    let medium = mediumInputWithParams || 'site';
+    
+    if (buttonType === 'qr') {
+        medium += '_QR';
+    }
+
+    const utmParams = {
+        source: source,
+        medium: medium,
+        content: content + '_' + buttonType.toUpperCase()
+    };
+
+    urlObjectWithParams.searchParams.set('utm_source', utmParams.source);
+    urlObjectWithParams.searchParams.set('utm_medium', utmParams.medium);
+    urlObjectWithParams.searchParams.set('utm_content', utmParams.content);
+
+    const urlParamsString = new URLSearchParams(urlObjectWithParams.searchParams).toString();
+    
+    outputUrlWithParams.innerHTML = `<p>Згенерований URL: <a href="${urlObjectWithParams.origin + urlObjectWithParams.pathname + '?' + urlParamsString}" target="_blank">${urlObjectWithParams.origin + urlObjectWithParams.pathname + '?' + urlParamsString}</a></p>`;
+}
