0cf0e40196ff1fc3c982c88881051cb34e3e1da0
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-02-20 17:27:12 +0200

    add content

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-
-
 function generateURL(buttonType) {
     const urlInput = document.querySelector('.urlInput').value;
-    const sourceInput = document.querySelector('.sourceInput').value;
-    const mediumInput = document.querySelector('.mediumInput').value;
+    let sourceInput = document.querySelector('.sourceInput').value;
+    let mediumInput = document.querySelector('.mediumInput').value;
+    let contentInput = document.querySelector('.contentInput').value;
     const outputUrl = document.querySelector('.outputUrl');
     const urlObject = new URL(urlInput);
-    const pathArray = urlObject.pathname.split('/');
+    let path = urlObject.pathname;
+
+
+    if (path.endsWith('/')) {
+        path = path.slice(0, -1);
+    }
+
+    const pathArray = path.split('/');
     
-    const content = pathArray[pathArray.length - 1].split('.')[0]; 
+    
+    sourceInput = sourceInput.trim() || urlObject.hostname.split('.')[0];
+    mediumInput = mediumInput.trim() || 'site';
+    contentInput = contentInput.trim() || pathArray[pathArray.length - 1].split('.')[0];;
+
+    if (buttonType === 'qr') {
+        mediumInput += '_QR';
+    }
 
     const utmParams = {
         source: sourceInput,
         medium: mediumInput,
-        content: content + '_' + buttonType.toUpperCase()
+        content: contentInput + '_' + buttonType.toUpperCase()
     };
 
     urlObject.searchParams.set('utm_source', utmParams.source);
     urlObject.searchParams.set('utm_medium', utmParams.medium);
     urlObject.searchParams.set('utm_content', utmParams.content);
-    
+
     outputUrl.innerHTML = `<p>Згенерований URL: <a href="${urlObject.toString()}" target="_blank">${urlObject.toString()}</a></p>`;
 }
 
